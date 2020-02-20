5a0a4c7927b9f9d29e00e2a5fb57890e89003eb5
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-02-19 16:00:23 -0800

    delete commented code

--- my_gui/myGui_step_123.py
+++ my_gui/myGui_step_123.py
@@ -32,23 +32,10 @@ class ParentWindow(Frame):
     # Buttons
         self.btn_browse1 = tk.Button(self.master,width=12,height=1,text='Browse...',command= self.getDir)
         self.btn_browse1.grid(row=1,column=0,padx=(20,0),pady=(70,0),sticky=W)
-##        self.btn_browse2 = tk.Button(self.master,width=12,height=1,text='Browse...')
-##        self.btn_browse2.grid(row=2,column=0,padx=(20,0),pady=(20,0),sticky=W)
 
-##        self.btn_check = tk.Button(self.master,width=12,height=3,text='Check for files...')
-##        self.btn_check.grid(row=3,column=0, columnspan=1, rowspan=2, padx=(20,0),pady=(20,0),sticky=W)
-        
-##        self.btn_close = tk.Button(self.master,width=12,height=3,text='Close Program')
-##        self.btn_close.grid(row=3,column=3, rowspan=2, padx=(450,0),pady=(20,0), sticky=E)
 
         self.txt_file1 = tk.Text(self.master)
         self.txt_file1.grid(row=1 ,column=1, columnspan=1, padx=(0, 50), pady=(0, 0))
-    #entry forms
-##        self.txt_file1 = tk.Entry(self.master, text='')
-##        self.txt_file1.grid(row=1 ,column=1, columnspan=3, padx=(20, 0), pady=(70, 0), sticky=W+E)
-##        self.txt_file2 = tk.Entry(self.master, text='')
-##        self.txt_file2.grid(row=2 ,column=1, columnspan=3, padx=(20, 0), pady=(20, 0), sticky=W+E)
-
 
 #============================Functionality================================================
 
